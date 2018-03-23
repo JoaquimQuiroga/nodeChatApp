3ffa08f79cdc6d426e3365d7348b3a086f2329a4
Joaquim Quiroga <joaquim.quiroga@phineal.com>
2018-03-23 09:07:12 -0300

    SOcket IO baseic

--- server/server.js
+++ server/server.js
@@ -1,11 +1,41 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
-const publicPath = path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, '..','public')
 const port = process.env.PORT || 3000
 let app = express()
+let server = http.createServer(app)
+let io = socketIO(server)
+
+
 app.use(express.static(publicPath))
 
-app.listen(port, ()=>{
-  console.log(`Server is up on ${port}`)
+
+//(register event listener
+io.on('connection', (socket) =>{
+  console.log('New user connected to socket')
+
+
+  socket.on('disconnect', ()=>{
+    console.log('User disconnected from socket')
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(port, () => {
+  console.log(`Server is up on : ${port}`)
 })
